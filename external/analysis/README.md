22cee9b8d3d3bf23708a5900f3983b7c447ced1a
Karthik Raman <karthik.raman@gmail.com>
2018-07-18 08:51:58 +0530

    lite

--- external/analysis/README.md
+++
@@ -1,1 +0,0 @@
-# <img src="https://prince.lcsb.uni.lu/img/icon_analysis.png" height="22px">&nbsp;&nbsp;External dependencies - analysis
--- external/base/utilities/slocstat/slocStatCOBRAToolbox.m
+++
@@ -1,5 +0,0 @@
-tmp=which('initCobraToolbox');
-folder=strrep(tmp,'initCobraToolbox.m','src/');
-[sloc,stat] = slocstat(folder);
-fprintf('Total number of lines %d for ''%s''\n',sloc,folder);
-printslocstat(stat);
